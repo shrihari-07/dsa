880ca9718865b053204c26a0223a5e5cc6b54d40
Shrihari K <shrihari.kowthal@gmail.com>
2025-08-26 21:20:46 +0530

    Add 94. Binary Tree Inorder Traversal

---
+++ tree/94. Binary Tree Inorder Traversal.py
@@ -0,0 +1,40 @@
+"""
+94. Binary Tree Inorder Traversal
+https://leetcode.com/problems/binary-tree-inorder-traversal/
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+
+Example 2:
+Input: root = [1,2,3,4,5,null,8,null,null,6,7,9]
+Output: [4,2,6,5,7,1,3,9,8]
+
+Example 3:
+Input: root = []
+Output: []
+
+Example 4:
+Input: root = [1]
+Output: [1]
+"""
+
+# Traversal: left -> root -> right
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        def recursiveTraversal(root):
+            if root is None:
+                return []
+            left_values = recursiveTraversal(root.left)
+            right_values = recursiveTraversal(root.right)
+            return [*left_values, root.val, *right_values]
+        
+        return recursiveTraversal(root)
