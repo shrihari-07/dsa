2affcb0cd27ddbbb8f75c66924c1cf62b93257c4
Shrihari K <shrihari.kowthal@gmail.com>
2026-04-23 19:54:55 +0530

    Add 42. Trapping Rain Water

---
+++ two-pointers/42. Trapping Rain Water.py
@@ -0,0 +1,35 @@
+"""
+42. Trapping Rain Water
+https://leetcode.com/problems/trapping-rain-water/
+
+Given n non-negative integers representing an elevation map where the width of each bar is 1, compute how much water it can trap after raining.
+
+Example 1:
+Input: height = [0,1,0,2,1,0,1,3,2,1,2,1]
+Output: 6
+Explanation: The above elevation map (black section) is represented by array [0,1,0,2,1,0,1,3,2,1,2,1]. In this case, 6 units of rain water (blue section) are being trapped.
+
+Example 2:
+Input: height = [4,2,0,3,2,5]
+Output: 9
+"""
+
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        left_height_max = height[left]
+        right_height_max = height[right]
+        water = 0
+
+        while left < right:
+            if left_height_max < right_height_max:
+                left += 1
+                left_height_max = max(left_height_max, height[left])
+                water += left_height_max - height[left]
+            else:
+                right -= 1
+                right_height_max = max(right_height_max, height[right])
+                water += right_height_max - height[right]
+            
+        return water
